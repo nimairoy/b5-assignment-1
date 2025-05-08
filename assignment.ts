f3e839749d3829096c8b31bef808b977eae4a8c4
Nimai Chandra Roy <nimairoy718@gmail.com>
2025-05-08 19:30:37 +0600

    problem 5 is solved

--- assignment.ts
+++ assignment.ts
@@ -1,4 +1,9 @@
 // Problem 1
+// Create a function that takes a string and an optional boolean.
+// If the boolean is true or not provided, return the string in uppercase.
+// If the boolean is false, return the string in lowercase.
+
+
 
 function formatString(input: string, toUpper: boolean = true ): string {
     return toUpper ? input.toUpperCase() : input.toLowerCase();
@@ -10,6 +15,7 @@ formatString('Bangladesh', false);
 
 
 // Problem 2
+// Create a function that filters an array of objects by the rating property, returning only items with a rating of 4 or more.
 
 type Items = {
     title: string;
@@ -44,7 +50,9 @@ const highRatedCourse = filterByRating(courses);
 console.log(highRatedCourse);
 
 
+
 // Problem 3
+// Create a generic function that concatenates multiple arrays of the same type using rest parameters.
 function concatenateArrays<T>(...arrays: T[][]): T[]{
     return  arrays.reduce((param1, param2) => param1.concat(param2), []);
 }
@@ -54,3 +62,60 @@ console.log(products);
 
 const numbers = concatenateArrays([1, 2, 3], [4, 2, 5, 2]);
 console.log(numbers);
+
+
+
+// Problem 4 
+// Create a Vehicle class with private make and year properties and a getInfo() method.
+// Create a Car class extending Vehicle, adding a private model property and a getModel() method.
+
+
+class Vehicle {
+    private make : string;
+    private year : number
+
+    constructor(make: string, year: number){
+        this.make = make;
+        this.year = year;
+    }
+
+    getInfo() : string{
+        return `Make : ${this.make},  Year: ${this.year}`;
+    }
+
+
+}
+
+class Car extends Vehicle {
+    private model : string;
+    constructor(make:string, year: number, model: string){
+        super(make, year);
+        this.model = model ;
+    }
+
+    getModel(): string {
+        return this.model;
+    }
+
+}
+
+const car1 = new Car('Toyota', 2018, 'Premeo F');
+console.log(car1.getInfo());
+console.log('Car model is :', car1.getModel());
+
+
+// Problem 5 
+// Write a function that takes a string | number and returns:
+// The length if it's a string
+// The number multiplied by 2 if it's a number
+
+function processInput(input: string | number): number {
+    if (typeof input === "string") {
+      return input.length;
+    } else {
+      return input * 2;
+    }
+  }
+  
+  console.log('String Length is: ', processInput('Next Level Web Developer'));
+  console.log('The Result is : ', processInput(50));
