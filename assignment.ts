9b60646051a35d8c7eae003e6fc54d738392aa5b
Nimai Chandra Roy <nimairoy718@gmail.com>
2025-05-08 08:56:14 +0600

    problem 2 is solved

--- assignment.ts
+++ assignment.ts
@@ -7,3 +7,38 @@ function formatString(input: string, toUpper: boolean = true ): string {
 formatString('Next Level Web Development Course');
 formatString('Bangladesh', true);
 formatString('Bangladesh', false);
+
+
+// Problem 2
+
+type Items = {
+    title: string;
+    rating: number;
+}
+
+function filterByRating(items: Items[]): Items[] {
+
+   return items.filter(item => item.rating >= 4);
+}
+
+const courses : Items[] = [
+    {
+        title : 'MERN Stack Web Development Course',
+        rating: 4.5
+    },
+    {
+        title : 'Wordpress Web Design Course',
+        rating: 3
+    },
+    {
+        title : 'Graphics Design Course',
+        rating: 4
+    },
+    {
+        title : 'Digital Marketing Course',
+        rating: 3.5
+    },
+]
+
+const highRatedCourse = filterByRating(courses);
+console.log(highRatedCourse);
